2dcfc18b41c2930fea13d8704b69ee42c24adcdf
varunwali <varunwali36@gmail.com>
2024-03-29 00:06:02 +0530

    commit

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -14,28 +14,39 @@ const Contact = () => {
           method="post"
           enctype="text/plain">
           <div className={styles.forminput}>
-            <input type="name" name="Name" placeholder="varun" required></input>
+            <input
+              type="name"
+              name="Name"
+              placeholder="varun"
+              required
+              autocomplete="given-name"></input>
           </div>
           <div className={styles.forminput}>
             <input
               type="email"
               name="Email"
               placeholder="varun@gmail.com"
-              required></input>
+              required
+              autocomplete="given-email"></input>
           </div>
           <div className={styles.forminput}>
             <input
               type="tel"
               name="Phone"
               placeholder="9876543219"
-              required></input>
+              required
+              autocomplete="given-number"></input>
           </div>
           <textarea
             name="message"
             rows={5}
             cols={20}
             placeholder="Message..."></textarea>
-          <input className={styles.submit} type="submit" value="Submit"></input>
+          <input
+            className={styles.submit}
+            type="submit"
+            value="Submit"
+            autocomplete="given-message"></input>
         </form>
       </div>
     </section>
